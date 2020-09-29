a8ea7758e7a304b648d44b3774fb273305572ef4
VSRUJANA <srujana.valavala@gmail.com>
2020-09-29 15:47:22 +0530

    [srujana] Add. Start Game program is added

--- Snake_And_Ladder_simulator/Program.cs
+++ Snake_And_Ladder_simulator/Program.cs
@@ -6,7 +6,13 @@ namespace Snake_And_Ladder_simulator
     {
         static void Main(string[] args)
         {
+            const int START_POINT = 0;
             Console.WriteLine("Welcome to Snake and Ladder Simulator !");
+            Console.WriteLine("Enter your name");
+            string playerName = Console.ReadLine();
+            int currentPosition = START_POINT;
+            Console.WriteLine("Your position is : " + currentPosition);
         }
+
     }
 }
