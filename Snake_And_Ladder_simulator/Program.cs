82d2948488798981c6b2d0572961c86c7dda2026
VSRUJANA <srujana.valavala@gmail.com>
2020-09-29 16:21:47 +0530

    [srujana] Add. Snake and Ladder game with two players

--- Snake_And_Ladder_simulator/Program.cs
+++ Snake_And_Ladder_simulator/Program.cs
@@ -10,48 +10,64 @@ namespace Snake_And_Ladder_simulator
         const int SNAKE = 2;
         const int NO_PLAY = 3;
         static int currentPosition = 0;
+        static int currentPosition1 = 0;
+        static int currentPosition2 = 0;
+
         static void Main(string[] args)
         {
-            int i = 0;
-            Console.WriteLine("Welcome to Snake and Ladder Simulator !");
-            do
+            Console.WriteLine("Welcome to snakes and ladders game");
+            Console.WriteLine("Enter player 1 name");
+            string player1 = Console.ReadLine();
+            Console.WriteLine("Enter player 2 name");
+            string player2 = Console.ReadLine();
+            while (currentPosition < 100)
             {
-                int numberRolled = Roll();
-                currentPosition = Movement(numberRolled);
-                i++;
-                Console.WriteLine("Dice Roll number " + i + " - " + " your position : " + currentPosition);
-
+                int score1 = Roll();
+                currentPosition = currentPosition1;
+                currentPosition1 = Movement(score1);
+                //Console.WriteLine("player 1 rolled " + score1 + " and is at " + currentPosition1);
+                int score2 = Roll();
+                currentPosition = currentPosition2;
+                currentPosition2 = Movement(score2);
+                //Console.WriteLine("player 2 rolled " + score2 + " and is at " + currentPosition2);
             }
-            while (currentPosition < 100);
-            Console.WriteLine("Game over! You rolled the dice " + i + " times to win the game");
-        }
-        static int Roll()
-        {
-            Random random = new Random();
-            int numberOnDice = random.Next(1, 7);
-            return numberOnDice;
-        }
-        static int Movement(int diceValue)
-        {
-            Random random = new Random();
-            int option = random.Next(1, 4);
-            switch (option)
+            if (currentPosition1 == 100)
             {
-                case LADDER:
-                    currentPosition += diceValue;
-                    if (currentPosition > 100)
-                        currentPosition -= diceValue;
-
-                    break;
-                case SNAKE:
-                    currentPosition -= diceValue;
-                    if (currentPosition < 0)
-                        currentPosition = 0;
-                    break;
-                case NO_PLAY:
-                    break;
+                Console.WriteLine(player1 + " won the game!");
             }
-            return currentPosition;
+            else
+            {
+                Console.WriteLine(player2 + " won the game!");
+            }
+
+            static int Roll()
+            {
+                Random random = new Random();
+                int diceValue = random.Next(1, 7);
+                return diceValue;
+            }
+            static int Movement(int score)
+            {
+                Random random = new Random();
+                int option = random.Next(1, 4);
+                switch (option)
+                {
+                    case LADDER:
+                        currentPosition += score;
+                        if (currentPosition > 100)
+                            currentPosition -= score;
+                        Movement(currentPosition);
+                        break;
+                    case SNAKE:
+                        currentPosition -= score;
+                        if (currentPosition < 0)
+                            currentPosition = 0;
+                        break;
+                    case NO_PLAY:
+                        break;
+                }
+                return currentPosition;
+            }
         }
     }
 }
